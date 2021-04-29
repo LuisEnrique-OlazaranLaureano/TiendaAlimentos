29b48a07651831ebe142a8fc712d265eed5fe463
Luis Enrique Olazarán Laureano <63483840+LuisEnrique-OlazaranLaureano@users.noreply.github.com>
2021-04-29 17:07:12 -0500

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,10 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">STORE &copy; 2021</h5>
+</footer>
