e5863d959c0f6b96bf2e1d2b3e6bd41533c46ccb
Luis Enrique Olazarán Laureano <63483840+LuisEnrique-OlazaranLaureano@users.noreply.github.com>
2021-04-29 17:24:48 -0500

    Add files via upload

---
+++ library/configServer.php
@@ -0,0 +1,5 @@
+<?php
+define("USER", "freedbtech_storeCarlosMauricio");#Nombre de Usuario de tu Phpmyadmin
+define("SERVER", "freedb.tech"); #Si trabajas de manera local deja localhost
+define("BD", "freedbtech_storeCarlosMauricio");//nombre de la base de datos
+define("PASS", "storeCarlosMauricio");//contraseña de tu usuario en phpmyadmin
